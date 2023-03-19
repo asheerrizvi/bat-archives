d883b8a5c99066b77d3dcea263c4a3bb07ffe09f
Asheer Rizvi <asheer.vil@gmail.com>
2023-03-19 10:14:32 +0530

    Improved filtering and added styling for active filter

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -8,21 +8,30 @@ import { MoviesService } from '../movies.service';
 })
 export class MoviesComponent {
   movies: MovieDetails[] = [];
+  filteredMovies: MovieDetails[] = [];
+  filteredDecade: number | undefined = undefined;
 
   constructor(private moviesService: MoviesService) {}
 
   ngOnInit(): void {
     this.moviesService.getMovies().subscribe((movies) => {
       this.movies = movies;
+      this.filteredMovies = movies;
     });
   }
 
-  filterMovies(decade: number) {
-    this.movies = this.movies.filter((movie) => {
-      const movieYears = movie.Year.split('-');
-      return movieYears
-        .map((year) => Number(year))
-        .every((year) => year >= decade && year < decade + 10);
-    });
+  filterMovies(decade?: number) {
+    if (!decade) {
+      this.filteredMovies = this.movies;
+      this.filteredDecade = undefined;
+    } else {
+      this.filteredMovies = this.movies.filter((movie) => {
+        const movieYears = movie.Year.split('-');
+        return movieYears
+          .map((year) => Number(year))
+          .every((year) => year >= decade && year < decade + 10);
+      });
+      this.filteredDecade = decade;
+    }
   }
 }
